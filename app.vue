37bab5dd0ba2ffe63b42fd1ab60608ddac81d611
lucas <lucas.pinho@policiacientifica.sc.gov.br>
2024-12-20 12:37:05 -0300

    Primeiro commit

---
+++ app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+
+<script setup>
+useSeoMeta({
+  title: 'PCI - Perito Lucas Pinho',
+  description: 'This is my amazing site, let me tell you all about it.',
+  ogDescription: 'This is my amazing site, let me tell you all about it.',
+})
+</script>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
---
+++ components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+      <nav>
+        <ul>
+          <li><NuxtLink to="/criminalsearch">CriminalSearch</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+  </template>
+  
+
+  <style scoped>
+.link {
+    @apply bg-gray-200
+}
+</style>
